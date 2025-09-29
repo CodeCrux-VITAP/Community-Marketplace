66c6f4b49e1e8c08f79f5a4108b523a220ab2865
Sushanth1425 <sushanthb1425@gmail.com>
2025-09-30 04:03:00 +0530

    updated chat schema in chat.js and added multer package in dependencies

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -1,11 +1,15 @@
 const mongoose= require('mongoose')
 
-const chatSchema= mongoose.Schema({
+const messageSchema= mongoose.Schema({
   sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  receiver: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  item: {type: mongoose.Schema.Types.ObjectId, ref: 'Item'},
+  text: {type: String, required: true},
   read: {type: Boolean, default: false},
-  content: {type: String, required: true},
-}, {timestamps: true})
+}, {timestamps: true} )
 
-module.exports= mongoose.model('Chat', chatSchema)
+const chatSchema= mongoose.Schema({
+  users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}],
+  item: {type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: false},
+  messages: [messageSchema]
+}, {timestamps: true} )
+
+module.exports= mongoose.model('Chat', chatSchema) 
