5c3fe1c5aa08a1280bf6218f6638c8cccc702602
Sushanth1425 <sushanthb1425@gmail.com>
2025-09-30 09:50:00 +0530

    added model, routes & web sockets for chats

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express= require('express')
 require('dotenv').config()
 const cors= require('cors')
 const cookieParser= require('cookie-parser')
+const http= require('http')
 
 const {connectDB}= require('./utils/db')
 const authRoutes= require('./routes/auth')
 const itemRoutes= require('./routes/item')
 const cartRoutes= require('./routes/cart')
 const wishlistRoutes= require('./routes/wishlist')
+const chatRoutes= require('./routes/chat')
 
 const app= express()
 
@@ -27,7 +29,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/items', itemRoutes)
 app.use('/api/cart', cartRoutes)
 app.use('/api/wishlist', wishlistRoutes)
+app.use('/api/chat', chatRoutes)
+
+const server= http.createServer(app)
+const io= require('./utils/webSocket')(server)
 
 const port= process.env.PORT || 5050;
 
-app.listen(port, ()=> console.log(`Server connected to port ${port}`))
+server.listen(port, ()=> console.log(`Server connected to port ${port}`))
