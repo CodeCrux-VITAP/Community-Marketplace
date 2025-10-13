12889d4a13ae3409174ba94f80f84c24709e8186
Sushanth1425 <sushanthb1425@gmail.com>
2025-10-13 16:34:59 +0530

    created app.jsx file with minimal features

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
+  </StrictMode>
+
 )
