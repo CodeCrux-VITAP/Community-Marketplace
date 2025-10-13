3e69a1c87c6a994d5f3eb78cf2ba4992aad50f97
Sushanth1425 <sushanthb1425@gmail.com>
2025-10-13 15:58:46 +0530

    added protected route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Navigate } from "react-router-dom";
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+const ProtectedRoute= ({children}) =>{
+  const {user}= useContext(AuthContext);
+  return user ? children : <Navigate to='/login' />
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
